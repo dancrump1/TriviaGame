c36396df1d7cfa39174075ce6564d7c14ba7047a
dancrump1 <42187710+dancrump1@users.noreply.github.com>
2018-10-19 14:28:47 -0400

    recieving questions from api after selecting difficulty

--- assets/javasript/app.js
+++ assets/javasript/app.js
@@ -1,33 +1,60 @@
 // Link to questions API: https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=multiple
 var APIurl;
-$(".btn").click(function(){
-var userDifficulty = $(this).val();
-if (userDifficulty == 'Easy') {
-    APIurl = "https://opentdb.com/api.php?amount=10&category=18&difficulty=" + userDifficulty + "&type=multiple";
-    $.ajax({
-        url: APIurl,
-        Method: "GET"
-    }).then(function (response) {
-        console.log(response);
-    })
-}
-else if (userDifficulty == 'Medium'){
-    APIurl = "https://opentdb.com/api.php?amount=10&category=18&difficulty=" + userDifficulty + "&type=multiple";
-    $.ajax({
-        url: APIurl,
-        Method: "GET"
-    }).then(function (response) {
-        console.log(response);
-    })  
-}
 
-else if (userDifficulty == 'Hard'){
-    APIurl = "https://opentdb.com/api.php?amount=10&category=18&difficulty=" + userDifficulty + "&type=multiple";
-    $.ajax({
-        url: APIurl,
-        Method: "GET"
-    }).then(function (response) {
-        console.log(response);
-    }) 
-}
-})
+
+
+
+
+
+
+
+
+
+
+$(".difficulty").on("click", function () {
+    var userDifficulty = $(this).val();
+    if (userDifficulty == 'easy') {
+        APIurl = "https://opentdb.com/api.php?amount=10&category=18&difficulty=" + userDifficulty + "&type=multiple";
+        $.ajax({
+            url: APIurl,
+            Method: "GET"
+        }).then(function (response) {
+            console.log(response);
+
+            var qaText = $("#questionList-Text").html(response.results)
+            var questionFromAPI = [];
+            var answerFromAPI;
+            var correctAnswer;
+            var incorrectAnswer1;
+            var incorrectAnswer2;
+            var incorrectAnswer3;
+
+            for (i = 0; i < 6; i++) {
+                questionFromAPI = response.results[i].question;
+                console.log(questionFromAPI);
+            }
+
+
+        })
+    }
+    else if (userDifficulty == 'medium') {
+        APIurl = "https://opentdb.com/api.php?amount=10&category=18&difficulty=" + userDifficulty + "&type=multiple";
+        $.ajax({
+            url: APIurl,
+            Method: "GET"
+        }).then(function (response) {
+            console.log(response);
+        })
+    }
+
+    else if (userDifficulty == 'hard') {
+        APIurl = "https://opentdb.com/api.php?amount=10&category=18&difficulty=" + userDifficulty + "&type=multiple";
+        $.ajax({
+            url: APIurl,
+            Method: "GET"
+        }).then(function (response) {
+            console.log(response);
+        })
+    }
+});
+
